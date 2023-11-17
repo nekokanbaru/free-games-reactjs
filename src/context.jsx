954ef3abe2e03f470abcec93c0bacb18400245e5
nekokanbaru <mihailorang@gmail.com>
2023-11-17 14:30:52 +0100

    14 delete redundant fetch

--- src/context.jsx
+++ src/context.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { useCallback, useContext, useEffect, useState} from "react";
-
-const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-    const options = {
-	    method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': '3b5d4c36b8mshd5546e81fd09131p150cfdjsn8a485f973c61',
-		    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-	    }
-    };
     
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
-    const [gameList, setGameList] = useState([])
     const [filteredGameList, setFilteredGameList] = useState([])
     const [isFiltered, setIsFiltered] = useState(false)
     const [categoryList, setCategoryList] = useState("mmorpg")
     const [platform, setPlatform] = useState("all")
-    const [isLoading, setIsLoading] = useState(false)
     const [isLoadingFilter, setIsLoadingFilter] = useState(false)
 
     const filterOptions = {
@@ -34,29 +23,6 @@ const AppProvider = ({children}) => {
         }
 
     }
-
-    const fetchGames = useCallback(async () => {
-        setIsLoading(true)
-        try{
-            const response = await fetch(url, options)
-            const data = await response.json()
-            if(data.length > 0) {
-                const gamesList = data.map((item) => {
-                    const {id, title, platform, genre, thumbnail, short_description} = item
-                    return {id, title, platform, genre, thumbnail, shortDesc:short_description}
-                })
-                setGameList(gamesList)
-                setIsLoading(false)    
-            }
-            else {
-                setGameList([])
-                setIsLoading(false)
-            }
-        }
-        catch(error){
-            console.log(error)
-        }
-    })
 
     const filterGames = useCallback(async () => {
         setIsFiltered(true)
@@ -83,14 +49,10 @@ const AppProvider = ({children}) => {
     })
 
     useEffect(() => {
-        fetchGames()
-    }, [])
-
-    useEffect(() => {
         filterGames()
     }, [categoryList, platform])
 
-    return <AppContext.Provider value={{gameList, filteredGameList, isFiltered, setCategoryList, setPlatform, isLoading, isLoadingFilter}}>{children}</AppContext.Provider>
+    return <AppContext.Provider value={{filteredGameList, isFiltered, setCategoryList, setPlatform, isLoadingFilter}}>{children}</AppContext.Provider>
 }
 
 export const useGlobalContext = () => {
