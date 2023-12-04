57804fa65510de874a1a04edce0e6c3a7df990a5
nekokanbaru <mihailorang@gmail.com>
2023-12-04 11:52:48 +0100

    game store page responsive
    need to add hamburger menu for sidebar on small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,25 @@
-import {FaShoppingCart, FaHeart, FaUser} from 'react-icons/fa'
+import {FaHeart, FaUser} from 'react-icons/fa'
 import '../styles/navbar.css' 
 
 
 export default function Navbar()
 {
     return <div className="navbar">
-        <div className="navbar-left list">
-            {/* replace with react router later on */}
+        {/* <div className="navbar-left list">
+            
             <ul>
                 <li>Home</li>
-                <li>Streams</li>
-                <li>Game Store</li>
-                <li>News</li>
             </ul>
         </div>
         <div className="navbar-right list">
             <ul>
-                <li><FaShoppingCart size={25} className='navbar-icon'/></li>
-                <li><FaHeart size={25} className='navbar-icon'/></li>
                 <li><FaUser size={25} className='navbar-icon'/></li>
             </ul>
-        </div>
+        </div> */}
+
+        {/* replace with logo later */}
+        <p>Home</p>
+        <FaUser size={25} className='navbar-icon'></FaUser>
     </div>
 }
 
