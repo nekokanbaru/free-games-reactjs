69884abd0fb5dc9b27145ce9f508d0f7529c4bfc
nekokanbaru <mihailorang@gmail.com>
2023-12-07 12:44:09 +0100

    94 add background image on 404 page
    only on desktop for now

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,11 +1,20 @@
 import {Link} from 'react-router-dom'
 import React from 'react'
 import '../styles/error.css'
+import background from '../assets/404.jpeg'
 
 export default function Error() {
-    return <div className="error-container">
+    return <div className="error-wrapper">
+        <div className="image-container">
+        <img src={background} alt="" />
+        <div className="image-shadow"></div>
+        </div>
+
+        <div className="error-container">
         <h1>404</h1>
         <p>the page was not found</p>
         <Link to={`/`}><button className='btn'>Back to home</button></Link>
+        </div>
+        
     </div>
 }
